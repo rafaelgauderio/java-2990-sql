691ab6c2c0a7dcf3dac84401938286981a13e30a
Rafael de Luca <deluca1712@gmail.com>
2022-11-08 11:31:22 -0300

    Criando projeto, entidades, dto

---
+++ src/main/java/com/rafaeldeluca/uri2990/projections/EmpregadoDeptProjection.java
@@ -0,0 +1,8 @@
+package com.rafaeldeluca.uri2990.projections;
+
+public interface EmpregadoDeptProjection {
+
+	String getCpf();
+	String getEnome();
+	String getDnome();
+}
---
+++ src/main/java/com/rafaeldeluca/uri2990/repositories/EmpregadoRepository.java
@@ -0,0 +1,9 @@
+package com.rafaeldeluca.uri2990.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.rafaeldeluca.uri2990.entities.Empregado;
+
+public interface EmpregadoRepository extends JpaRepository<Empregado, Long> {
+
+}
